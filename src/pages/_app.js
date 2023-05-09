496e16dce6db74b725f6e2cb6b8dfeed66bb8dce
shimon yoshida <sssssss1025@gmail.com>
2023-05-09 13:20:49 +0900

    ビューポート調整

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { M_PLUS_2 } from "@next/font/google";
+import Head from 'next/head'
 
 const inter = M_PLUS_2({
   subsets: ['latin'],
@@ -7,9 +8,13 @@ const inter = M_PLUS_2({
   display: "swap",
 });
 export default function App({ Component, pageProps }) {
-  return (
-    // <main className="--font-inter">
+  return (<>
+    {/* // <main className="--font-inter"> */}
+    <Head>
+      <meta name="viewport" content="width=device-width"></meta>
+    </Head>
       <Component {...pageProps} />
-    // </main>
+    {/* // </main> */}
+    </>
   );
 }
